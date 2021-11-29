806d053e610c21f16266c8e4073b08ea8b066976
Bartek Chaber <bartosz.chaber@ee.pw.edu.pl>
2021-11-29 12:03:48 +0100

    Improved gradient calculations based on SVectors

--- gradient.jl
+++ gradient.jl
@@ -6,26 +6,31 @@ struct GradientOperator{T <: Tuple} # 1D, only x-coord
     bnds :: Tuple{Symbol, Symbol}
 end
 
-calculate_gradient!(dudx, u, δx, n, boundary, bc::PeriodicBC) = begin
-    if boundary == :left  @inbounds dudx[1]   = @views (u[1] - u[n]) / (first(δx) + last(δx)) end
-    if boundary == :right @inbounds dudx[n+1] = @views (u[1] - u[n]) / (first(δx) + last(δx)) end
-end
+@inline grad(u, δx, i, bnd::Type{Val{:Left}},  bc::PeriodicBC) = (first(u) - last(u)) / (first(δx) + last(δx))
+@inline grad(u, δx, i, bnd::Type{Val{:Right}}, bc::PeriodicBC) = (first(u) - last(u)) / (first(δx) + last(δx))
 
-calculate_gradient!(dudx, u, δx, n, boundary, bc::DirichletBC) = begin
-    if boundary == :left  @inbounds dudx[1]   = @views (u[1] - bc.value)  / first(δx) end
-    if boundary == :right @inbounds dudx[n+1] = @views (bc.value - u[n]) / last(δx) end
-end
+@inline grad(u, δx, i, bnd::Type{Val{:Left}},  bc::DirichletBC) = (first(u) - bc.value) / first(δx)
+@inline grad(u, δx, i, bnd::Type{Val{:Right}}, bc::DirichletBC) = (bc.value -  last(u)) / last(δx)
 
-calculate_gradient!(dudx, u, δx, n) = @inbounds dudx[2:n] .= @views (u[2:n] .- u[1:n-1]) ./ δx[2:n]
+@inline grad(u, δx, i) = (u[i] - u[i-1]) / δx[i]
 
-function (∇::GradientOperator)(u; result=nothing, reverse=false)
-    dudx = isnothing(result) ? zeros(∇.nx + 1) : result
-    
-    calculate_gradient!(dudx, u, ∇.δx, ∇.nx)
-    for (bnd, bc) in zip(∇.bnds, ∇.bcs)
-        calculate_gradient!(dudx, u, ∇.δx, ∇.nx, bnd, bc)
+function (∇::GradientOperator)(u::Vector; result=nothing)
+    ∇u = isnothing(result) ? zeros(SVector{3, eltype(u)}, ∇.nx + 1) : result
+    n  = ∇.nx
+    left,right = ∇.bcs
+    dx, dy, dz = 0.0, 0.0, 0.0
+    for i=1
+        dx = grad(u, ∇.δx, i, Val{:Left},   left)
+        ∇u[i] = @SVector [dx, dy, dz]
+    end
+    for i=n+1
+        dx = grad(u, ∇.δx, i, Val{:Right}, right)
+        ∇u[i] = @SVector [dx, dy, dz]
+    end
+    for i=2:n
+        dx = grad(u, ∇.δx, i)
+        ∇u[i] = @SVector [dx, dy, dz]
     end
     
-    if reverse dudx .*= -1.0 end
-    return dudx
+    return ∇u
 end
